476eb0718192a0ccb6c8187aa33890a758529839
amorphobia <pengxuesong.cn@gmail.com>
2020-06-23 16:00:30 +0800

    Copy flow from ad-m/github-push-action

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+INPUT_CONTENTS=${INPUT_CONTENTS:-contents/*.txt}
+INPUT_DICT=${INPUT_DICT:-shupin.dict.yaml}
+INPUT_BRANCH=${INPUT_BRANCH:-master}
+
+[ -z "${INPUT_TOKEN}" ] && {
+  echo 'Missing input "token: ${{ secrets.GITHUB_TOKEN }}".';
+  exit 1;
+};
+
+echo "Basic info"
+echo "  Contents"
+ls -lh ${INPUT_CONTENTS}
+echo "  Dict"
+ls -lh ${INPUT_DICT}
+echo "  GitHub Actor: ${GITHUB_ACTOR}"
+echo "  Repo: ${GITHUB_REPOSITORY}"
+
+echo "Perform merging contents"
+cat ${INPUT_CONTENTS} > ${INPUT_DICT}
+
+echo "Test modified"
+if [ "$(git diff ${INPUT_DICT})" != "" ]; then
+  export MODIFIED=true
+fi
+
+if [ $MODIFIED == "true" ]; then
+  echo "Commit files"
+  git config --local user.email "action@github.com"
+  git config --local user.name "GitHub Action"
+  git add ${INPUT_DICT}
+  git commit -m "Merging contents"
+
+  echo "Push changes"
+  git push "https://${INPUT_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:${INPUT_BRANCH}
+fi
